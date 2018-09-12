9fb793aa8979045c99b2004e26d80f41851a810d
Уринбасаров Диас Нуржанулы <@corp.itstep>
2018-09-12 18:25:00 +0600

    First try

---
+++ EssentialElements/Fiber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialElements
+{
+    public class Fiber
+    {
+        public string Name { get; set; }
+
+        //public Fiber[] fibers = 
+
+
+    }
+}
